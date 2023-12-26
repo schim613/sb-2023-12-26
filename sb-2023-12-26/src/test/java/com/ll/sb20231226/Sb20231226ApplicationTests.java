06d9601a69e270b85473f341eec61fbe509b2be0
schim613 <uximin@naver.com>
2023-12-26 16:02:15 +0900

    setting

---
+++ sb-2023-12-26/src/test/java/com/ll/sb20231226/Sb20231226ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sb20231226;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb20231226ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
